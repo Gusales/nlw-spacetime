59816911780defa16cd1d459fa1d7af5a40293f9
Gusales <gussales14@gmail.com>
2023-05-15 22:38:12 -0300

    Docs: setup frontend aplication
    Aplication reactjs using framework nextjs version 13

---
+++ web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-zinc-950">
+      <h1 className="text-2xl font-bold uppercase text-white">Hello World</h1>
+    </div>
+  )
+}
