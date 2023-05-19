115d65f457f04f354a56d714fb0655ae6ae00906
Gusales <gussales14@gmail.com>
2023-05-18 22:53:51 -0300

    Docs: create route for file upload

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,9 +4,12 @@ import fastify from 'fastify'
 
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 
+import { resolve } from 'node:path'
 import { authRotes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
 const port = process.env.PORT ? Number(process.env.PORT) : 3333
@@ -16,12 +19,20 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(multipart)
+
 app.register(jwt, {
   secret,
 })
 
-app.register(memoriesRoutes)
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+
 app.register(authRotes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
 
 app
   .listen({
